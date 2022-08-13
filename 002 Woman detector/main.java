4f67abf8378cc70adf0374919c45c876c88506cb
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-08-13 18:40:12 +0200

    Create main.java

---
+++ 002 Woman detector/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean areYouAWoman(String name)
+    {
+        return name.endsWith("a");
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write your name");
+        String name = scanner.nextLine();
+
+        if(areYouAWoman(name))
+        {
+            System.out.println("You are a woman");
+        }
+        else
+        {
+            System.out.println("You are a man");
+        }
+    }
+}
